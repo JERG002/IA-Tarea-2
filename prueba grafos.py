73306686c36430a74e23de87853ada3428d0363e
JERG002 <julian.ramgarza@gmail.com>
2023-02-12 22:30:07 -0600

    Funciona pero solo para grafos de 4 nodos o menos

---
+++ prueba grafos.py
@@ -0,0 +1,48 @@
+import random
+import grafo_ponderado as GRP
+
+def generador_grafo(nodos):
+    instancia = {}
+    for i in range(1,nodos+1):
+        nodos_adyacentes = random.randint(2, nodos-1)
+        lista_nodos_adyacentes = random.sample(range(1,nodos), nodos_adyacentes)
+        while(i in lista_nodos_adyacentes):
+            nodos_adyacentes = random.randint(2,nodos-1)
+            lista_nodos_adyacentes = random.sample(range(1,nodos), nodos_adyacentes)
+        
+        lista_tuplas = []
+        for j in range(0, len(lista_nodos_adyacentes)):
+            lista_tuplas.append((str(lista_nodos_adyacentes[j]),random.randint(1,1000)))
+        instancia[str(i)] = lista_tuplas
+
+
+    for i in instancia:
+        valida = 0
+        JJ = -1
+        for j in instancia[i]:
+            JJ = JJ + 1
+            for x in instancia:
+                if x != i:
+                    if i == instancia[x][JJ][0]:
+                        valida = 1
+        if valida == 0:
+            insertar  = str(random.randint(1, nodos))
+            tupla_insertada = (i, random.randint(1,1000))
+            while i == insertar:
+                insertar  = str(random.randint(1, nodos))
+            instancia[insertar].insert(0,tupla_insertada)
+
+    return instancia
+
+
+    
+
+
+grafo = generador_grafo(5)
+
+print(grafo)
+g = GRP.WeightedGraph(grafo)
+
+
+path, weight = g.shortestPath('1','4')
+print(f'Dijkstra: ruta D-G:{path} peso:{weight}')
